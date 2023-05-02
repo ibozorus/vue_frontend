b17c0f6044bb6372fd36ffb8212b43b8cf8d96e0
ibozorus <korkmazie@gmail.com>
2023-05-02 17:16:45 +0200

    Initial COmmit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data() {
+        return {
+            allBenutzer: []
+        }
+    },
+    created() {
+        this.fetchAllUsers();
+    },
+    methods: {
+        async fetchAllUsers() {
+            await fetch("http://localhost:8081/api/v1/benutzer").then((response) => {
+                response.json().then((data) => {
+                    this.allBenutzer = data;
+                    console.log(data);
+                });
+            });
+        }
+    }
+}
+</script>
+<template>
+
+            <div v-for="benutzer in allBenutzer" :id="benutzer.id">
+                <p>Benutzername</p><span>{{benutzer.benutzername}}</span>
+                <p>E-Mail</p><span>{{benutzer.email}}</span>
+            </div>
+
+
+</template>

ab40d5e974846d5889a73fef8c46d6bae5949d3d
ibozorus <korkmazie@gmail.com>
2023-05-04 23:25:08 +0200

    Login fertig Register angefange

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script>
 
 import Login from './components/LoginPage.vue'
+import Register from './components/RegisterPage.vue'
+
 export default {
 
     components: {
-        "login" :Login
+        "login": Login,
+        "register": Register
     },
     data() {
-        return {}
+        return {
+            activeComp: 'login'
+        }
     },
     created() {
     },
@@ -15,6 +20,30 @@ export default {
 }
 </script>
 <template>
-    <login/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" @click="activeComp = 'login'">Login <span class="sr-only"></span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" @click="activeComp = 'register'">Register <span class="sr-only"></span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div>
+        <component :is="activeComp"></component>
+    </div>
 </template>
+<style>
+a:hover {
+    cursor: pointer;
+}
+</style>

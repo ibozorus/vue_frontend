1b429457a271698e499cd6e165d3e6f447a57520
ibozorus <korkmazie@gmail.com>
2023-05-04 12:25:17 +0200

    s

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script>
+import {preventOverflow} from "@popperjs/core";
+
 export default {
     data() {
         return {
-            allBenutzer: []
+            allBenutzer: [],
+            benutzername: "",
+            password: ""
         }
     },
     created() {
@@ -16,6 +20,12 @@ export default {
                     console.log(data);
                 });
             });
+        },
+        login(e) {
+            e.preventDefault();
+            for(let x in this.allBenutzer){
+                if(x.ben)
+            }
         }
     }
 }
@@ -25,6 +35,17 @@ export default {
     <div v-for="benutzer in allBenutzer" :id="benutzer.id">
         <p>Benutzername</p><span>{{ benutzer.benutzername }}</span>
         <p>E-Mail</p><span>{{ benutzer.email }}</span>
+    </div>
+    <div>
+        <form>
+            <label for="benutzername"><b>Benutzername</b></label>
+            <input v-model="this.benutzername" type="text" placeholder="Enter Username" name="benutzername" required>
+
+            <label for="kennwort"><b>Benutzername</b></label>
+            <input v-model="this.password" type="password" placeholder="Enter Password" name="kennwort" required>
+
+            <button @click="login($event)" id="login">Einloggen</button>
+        </form>
     </div>
 
 </template>
